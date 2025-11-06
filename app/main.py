28ef60389218856cf7eccde10052a453df2cca81
Jean F. Vanz <132222224+jeanvanz@users.noreply.github.com>
2025-11-06 00:14:27 +0000

    feat: Adiciona modelo e atualiza dependências

--- app/main.py
+++ app/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return jsonify({"message": "Hello World, Flask API is running!"})
 
+@app.route('/health-check', methods=['GET'])
+def health():
+    return jsonify({"status": "OK"})
+
 if __name__ == '__main__':
     app.run(debug=True)
